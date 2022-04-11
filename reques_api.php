5e93ed3614ca5df4466ac52a8b7cf39eba0d9441
Dumilson <=>
2022-04-11 10:13:10 +0100

    Retornando as coordenadas

--- reques_api.php
+++ reques_api.php
@@ -1,15 +1,15 @@
 
 <?php
 
-$url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&input=cazenga%20de%20Maio&inputtype=textquery&key=AIzaSyCsJ8vVyCoHrvZuGxuhcwhlDEZtevVyoo8";
+$url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&input=cazenga%20de%20Maio&inputtype=textquery&key=AIzaSyCsJ8vVyCoHrvZuGxuhcwhlDEZtevVyoo8';
 $hg = file_get_contents($url);
-$response = json_decode(json_encode($hg));
-return $response;
-$get_location = $pokemons->candidates[0]->geometry->location;
-
+$response = json_decode($hg);
+$get_location = $response->candidates[0]->geometry->location;
 $send_request = [
-    "lat" => $get_location->lat,
-    "lng" => $get_location->lng,
+    'lat' => $get_location->lat,
+    'lng' => $get_location->lng,
 ];
+
+return $send_request;
 
 ?>
