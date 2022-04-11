70f3975ce42464a3fe15d8afa895c0d408963cd3
Dumilson <=>
2022-04-11 09:53:49 +0100

    Inicializando o proejcto de Localização de Atms

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Place Search Pagination</title>
+  <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+  <link rel="stylesheet" type="text/css" href="./style.css" />
+  <script src="./index.js"></script>
+</head>
+
+<body>
+  <div id="container">
+    <div id="map"></div>
+    <div id="sidebar">
+      <h2>Restultados</h2>
+      <div>
+        <form action="" method="post" id="form">
+          <input type="text" placeholder="Insira a Localização mais proxima" id="input_location">
+        </form>
+      </div>
+      <ul id="places"></ul>
+      <button id="more">Mais Resultados</button>
+    </div>
+  </div>
+
+  <!-- Async script executes immediately and must be after any DOM elements used in callback. -->
+  <script
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCsJ8vVyCoHrvZuGxuhcwhlDEZtevVyoo8&callback=initMap&libraries=places&v=weekly"
+    async></script>
+
+  <script>
+
+    fetch("reques_api.php")
+      .then((resp) => resp.json())
+      .then(function (data) {
+        console.log(data)
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  </script>
+</body>
+
+</html>
