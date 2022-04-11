3b90a163dc450f17ad8e0c2992553ecc1f1c25db
Dumilson <domingosbreganha9@gmail.com>
2022-04-11 09:04:07 +0100

    Consumir api com file_contents

--- reques_api.php
+++ reques_api.php
@@ -1,23 +1,15 @@
 
 <?php
 
-$url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&input=cazenga%20de%20Maio&inputtype=textquery&key=AIzaSyCsJ8vVyCoHrvZuGxuhcwhlDEZtevVyoo8';
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-$pokemons = json_decode(curl_exec($ch));
-
+$url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&input=cazenga%20de%20Maio&inputtype=textquery&key=AIzaSyCsJ8vVyCoHrvZuGxuhcwhlDEZtevVyoo8";
+$hg = file_get_contents($url);
+$response = json_decode(json_encode($hg));
+return $response;
 $get_location = $pokemons->candidates[0]->geometry->location;
 
 $send_request = [
-    'lat' => $get_location->lat,
-    'lng' => $get_location->lng,
-];
-
-return '{error:true}';
-
-return [
-  'data' => json_encode($send_request),
+    "lat" => $get_location->lat,
+    "lng" => $get_location->lng,
 ];
 
 ?>
